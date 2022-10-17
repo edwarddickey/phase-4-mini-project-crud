ec342bd8dc1e1f302c432cae445ccdc5bdee635a
Edward Dickey <edward.c.dickey@gmail.com>
2022-10-17 09:13:51 -0600

    commit tree 1728d09c0d7ff8a0da8bc5c16c3731a95fa34a3f

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,38 @@
+class SpicesController < ApplicationController
+    def index
+        spices = Spice.all
+        render json: spices
+      end
+    
+      # GET /spices/:id
+      def show
+        spice = Spice.find_by(id: params[:id])
+        render json: spice
+      end
+    
+      # POST /spices
+      def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+      end
+    
+      def update
+        spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render json: spice
+      end
+    
+      # DELETE /spices/:id
+      def destroy
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        head :no_content
+      end
+    
+    
+      private
+    
+      def spice_params
+        params.permit(:name, :image, :price, :is_in_stock)
+      end
+end
